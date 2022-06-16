9123344604bde287e9788f3eded3be0206e9f22c
GabbyMcCarthy <gah1289@gmail.com>
2022-06-16 10:38:40 -0400

    step 8, added session to store responses, fixed flash messages not showing up

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, redirect, request,flash
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
+from flask import session, make_response
 from surveys import surveys
 
 app=Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hopper'
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 
 survey_idx=0 
@@ -24,7 +25,10 @@ def get_questions():
 @app.route('/start', methods=["POST"])
 def first_question():
     """show first question of the selected survey"""
+    session.clear()
     responses.clear()
+    session['responses']=responses
+    # start with an empty response list and clear sessions every time you start
     list_i=request.form["survey_list_num"]
     survey_idx=list_i
     print(f"Survey index is {survey_idx}")
@@ -34,23 +38,27 @@ def first_question():
 @app.route('/questions/<int:qid>')
 def show_question(qid):
     """Show question and choices"""
+    if(len(responses)!=qid):
+        flash(f"Cannot answer questions out of order!")
+        # flash not appearing, but appears in sessions
+        return redirect(f'/questions/{len(responses)}')
     question=survey.questions[qid].question
-    choices=survey.questions[qid].choices   
+    choices=survey.questions[qid].choices
     return render_template('questions.html', question_num=qid, question=question, choices=choices, id=qid)
     
 @app.route('/next', methods=['POST'])
 def store_answer():
     """store the answer and redirect to next question"""
     try: 
-        answer=request.form['answer']        
+        answer=request.form['answer']
+            
     except: 
-        answer=None
-    if answer==None:
         flash("Please select an answer!")
-    # try/except not working, I want to go over this. I want to throw an error if the user forgets to select an answer.
-    
+        return redirect(f'/questions/{len(responses)}')  
+   
     else:
         responses.append(answer)
+        session['responses']=responses   
         qid=len(responses)
         print(responses)
         survey=(list(surveys.values()))[survey_idx]
@@ -59,4 +67,4 @@ def store_answer():
             return redirect(f"/questions/{qid}")
         else:
             return render_template('done.html')
-
+      
